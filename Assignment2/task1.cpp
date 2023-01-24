0c614c2b1876c4aca9b10cb974f604b5389c4ee5
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-01-24 21:44:22 +0200

    feat: Solution for Assignment 2 for elzero c++ course

---
+++ Assignment2/task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int age = 40,
+    //     period = 10;
+    // cout << "My Age Is: " << age << "\n";
+    // cout << period << " Years Ago My Age Was: " << age - period << "\n";
+    // cout << "After " << period << " Years My Age Will Be: " << age + period;
+    cout << "Bello\rHello Elzeroo\b";
+    cout << " Web Schoolll\b\b";
+    cout << " I Love Programming";
+    cout << " Too Much";
+    cout << " Specially C++\n";
+    return 0;
+}
