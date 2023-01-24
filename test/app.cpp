341c4226e3b59b90d477ec52a137515c3b478a8e
marwan-mohamed12 <marawanmohamed9876@gmail.com>
2023-01-24 21:44:55 +0200

    common playground

---
+++ test/app.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int price = 590;
+    // cout << price;
+    // cout << "I'm Learning Programming\n";
+    // cout << "With\n";
+    // cout << "Elzero\n";
+    // cout << "Web\n";
+    // cout << "School\n";
+    // cout << "<3\n";
+    // cout << "|=======================|\n";
+    // cout << "|== Elzero Web School ==|\n";
+    // cout << "|==        C++        ==|\n";
+    // cout << "|=======================|\n";
+    int j, k, l, m;
+    j = k = l = 10;
+    cout << j + k + l << endl;
+    cout << m;
+    return 0;
+}
